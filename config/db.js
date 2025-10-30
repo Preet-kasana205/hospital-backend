77ee94c798ac8079f011953ff8996c83cf5cfdcc
Preet-kasana205git config --global user.email kasanap553@gmail.com <kasanap553@gmail.com>
2025-10-30 15:38:27 +0530

    db

--- config/db.js
+++ config/db.js
@@ -8,7 +8,10 @@ const connectDB = async () => {
       process.exit(1);
     }
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected Successfully");
+   console.log("✅ MongoDB Connected Successfully");
+   console.log("Connected to DB:", mongoose.connection.name);
+   console.log("Connected Host:", mongoose.connection.host);
+
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error.message);
     // Exit process with failure
